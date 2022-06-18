e153e95e80ff6335861bc3ba01048b8015b3a929
mandeep-12345 <mandiip.dass1997@gmail.com>
2022-06-18 17:05:43 +0530

    message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,59 @@
-// import logo from './logo.svg';
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import Header from './component/Header';
-import Sidemenu from './component/sidemenu';
-import Navbar from './component/Navbar'
-import GridData from "./forms/GridData";
+// import Basic from "./component/forms/basicinfo/Basic";
+import Basic from "./component/forms/basicinfo/Basic";
+import Property from "./component/PropertyPage";
+import Signin from "./component/SignIn";
+import Signup from "./component/SignUp";
+import Propertypage from "./component/forms/propertydetails/Property";
+import Location from "./component/forms/Location_info/Location";
+import General from "./component/forms/General_info/General";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    UserID: localStorage.getItem("UserID"),
+    UserName: localStorage.getItem("UserName"),
+  });
+  const [data, setData] = useState({
+    PPID: "NA",
+    Property: "NA",
+    Area: "na",
+    Contact: "na",
+    Views: "na",
+    Duration: "na",
+  });
+
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-          <Route path="/Header" element={<Header />} />
-          <Route path="/Sidemenu" element={<Sidemenu />} />
-          <Route path="/Navbar" element={<Navbar />} />
-          <Route path="/GridData" element={<GridData />} />
+        <Route path="/*" element={<Signin setUser={setUser} />} />
+        <Route path="/Signin" element={<Signin setUser={setUser} />} />
+        {/* login page */}
+        <Route path="/SignUp" element={<Signup />} />
+        {/* registration page */}
+        <Route path="/Property" element={<Property user={user} />} />
+        {/* property page */}
+
+        <Route
+          path="/Basic"
+          element={<Basic user={user} data={data} setData={setData} />}
+        />
+        <Route
+          path="/PropertyDetails"
+          element={<Propertypage user={user} data={data} setData={setData} />}
+        />
+        <Route
+          path="/General"
+          element={<General user={user} data={data} setData={setData} />}
+        />
+        <Route
+          path="/location"
+          element={<Location user={user} data={data} />}
+        />
       </Routes>
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/component/SearchBAr.jsx
+++ frontend/src/component/SearchBAr.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 // import { MDBCol, MDBIcon } from "mdbreact";
-import './Searchbar.css'
+import './search.css'
 import serach from '../images/search.png'
 import add from '../images/Add_plus.png'
 import line from  '../images/Line.png'
+import {Link}  from "react-router-dom"
 
-
-const SearchBAr = () => {
+const SearchBAr = ({searchkey, setSearchKey}) => {
 
   return (
     <div className='searchbar-container'>
 
   <form className="form">
-  <input className="form-control" type="text"  placeholder="Search PPD ID" aria-label="Search"  />
+  <input className="form-control" type="text" value={searchkey} onChange={(e)=>{setSearchKey(e.target.value)}} placeholder="     Search PPID" aria-label="Search"  />
   <img className='line' src={line} alt='dta'/> 
 
   <img id='search'  src={serach} alt='data'/>
 
 </form>
 
-        
+        <Link to="/Basic">
         <button id='search-btn'>
           <img className='plus' src={add} alt='data'/> 
           Add Property
           </button>
-       
+        </Link>
 
         </div>
   )
